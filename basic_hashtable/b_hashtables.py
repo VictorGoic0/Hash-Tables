652602864983fcec81ceb12a82ecbbb7340e6035
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-08 17:02:57 -0400

    Imported DLL. Rewrote insert and remove methods to use DLL implementation.

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -1,18 +1,69 @@
 
-
-# '''
-# Basic hash table key/value pair
-# '''
 class Pair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
 
+class ListNode:
+  def __init__(self, value, prev=None, _next=None):
+    self.value = value
+    self.prev = prev
+    self.next = _next
 
-# '''
-# Basic hash table
-# Fill this in.  All storage values should be initialized to None
-# '''
+  def insert_after(self, value):
+    current_next = self.next
+    self.next = ListNode(value, self, current_next)
+    if current_next:
+      current_next.prev = self.next
+
+  def insert_before(self, value):
+    current_prev = self.prev
+    self.prev = ListNode(value, current_prev, self)
+    if current_prev:
+      current_prev.next = self.prev
+
+  def delete(self):
+    if self.prev:
+      self.prev.next = self.next
+    if self.next:
+      self.next.prev = self.prev
+
+class DoublyLinkedList:
+  def __init__(self, node=None):
+    self.head = node
+    self.tail = node
+    self.length = 1 if node is not None else 0
+
+  def remove_from_head(self):
+    current_head = self.head
+    self.delete(self.head)
+    self.head = current_head.next
+    return current_head.value
+
+  def add_to_tail(self, value):
+    if self.tail:
+      current_tail = self.tail
+      self.tail.insert_after(value)
+      self.tail = ListNode(value, current_tail)
+      current_tail.next = self.tail
+    else:
+      self.tail = ListNode(value)
+      self.head = self.tail
+    self.length += 1
+
+  def delete(self, node):
+    if node.prev == None:
+      self.head = node.next
+    elif node.next == None:
+      self.tail = node.prev
+    node.delete()
+    self.length -= 1
+    if (self.length == 0):
+      self.head = None
+      self.tail = None
+
+
+
 class BasicHashTable:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -39,8 +90,14 @@ def hash_table_insert(hash_table, key, value):
     hashed = hash(key)
     index = hashed % hash_table.capacity
     if index in hash_table.storage:
-        print(f"Overwriting {hash_table.storage[index]}")
-    hash_table.storage[index] = Pair(key, value)
+        current_bucket = hash_table.storage[index]
+        new_pair = Pair(key, value)
+        current_bucket.add_to_tail(new_pair)
+    else:
+        new_pair = Pair(key, value)
+        new_bucket = DoublyLinkedList()
+        new_bucket.add_to_tail(new_pair)
+        hash_table.storage[index] = new_bucket
 
 
 # '''
@@ -52,7 +109,21 @@ def hash_table_remove(hash_table, key):
     hashed = hash(key)
     index = hashed % hash_table.capacity
     if index in hash_table.storage:
-        hash_table.storage[index] = None
+        if hash_table.storage[index].length == 1:
+            hash_table.storage[index] = None
+        else:
+            # loop through, look for the key
+            current_node = hash_table.storage[index].head
+            found_key = False
+            while current_node:
+                if current_node.value.key == key:
+                    hash_table.storage[index].delete(current_node)
+                    found_key = True
+                    break
+                else:
+                    current_node = current_node.next
+            if not found_key:
+                print("Warning: Key not found")
     else:
         print("Warning: Key not found")
 
